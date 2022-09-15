24320432d243d60c3f6bad6e10733bd580c2608a
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-09-15 03:49:08 -0500

    backend set up with images

---
+++ mushroom-project/app/controllers/images_controller.rb
@@ -0,0 +1,6 @@
+class ImagesController < ApplicationController
+
+  def index
+    render json: Image.all
+  end
+end
--- mushroom-project/db/seeds.rb
+++ mushroom-project/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "faking images"
+
+10.times do |image|
+  Image.create(src: Faker::LoremFlickr.image(size: "50x60", search_terms: ['mushroom']))
+end
